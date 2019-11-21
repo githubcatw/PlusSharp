697785227b9a08ff2829aabedc9601ceff2b40d5
Narek Torosyan <narek.email@gmail.com>
2019-11-21 18:57:31 +0400

    Add random exts

---
+++ Assets/PlusSharp/Scripts/RndExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NUDev.PlusSharp {
+    public static class RndExtension  {
+        /// <summary>
+        /// Returns a random string. Don't use for secure stuff.
+        /// </summary>
+        /// <param name="length">The length of the string.</param>
+        /// <param name="chars">What characters to include.</param>
+        /// <returns>A random string.</returns>
+        public static string RandomString(this Random rnd, int length, string chars) {
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[Random.Range(0, s.Length)]).ToArray());
+        }
+
+        /// <summary>
+        /// Returns a random string. Don't use for secure stuff.
+        /// </summary>
+        /// <param name="length">The length of the string.</param>
+        /// <returns>A random string.</returns>
+        public static string RandomString(this Random rnd, int length) {
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length)
+              .Select(s => s[Random.Range(0, s.Length)]).ToArray());
+        }
+    }
+}
---
+++ Assets/Test/Rnd.cs
@@ -0,0 +1,20 @@
+﻿using NUDev.PlusSharp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(new Random().RandomString(7));
+        Debug.Log(new Random().RandomString(7, "ABCDEFG"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

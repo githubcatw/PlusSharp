cb872de8b6a8b087591e3c206945abf7e5daf9a8
Narek Torosyan <narek.email@gmail.com>
2020-02-25 22:26:39 +0400

    Unity 2019.3 and Vector Extensions

---
+++ Assets/PlusSharp/Editor/MoveGameCameraToEditorCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace NUDev.PlusSharp.Editor
+{
+    public class MoveGameCameraToEditorCamera : EditorWindow
+    {
+        [MenuItem("Window/Move Game camera to Scene View camera %m")]
+        // Use this for initialization
+        static void Init()
+        {
+            var cam = SceneView.lastActiveSceneView.camera;
+            Camera.main.transform.position = cam.transform.position;
+            Camera.main.transform.rotation = cam.transform.rotation;
+            //Capture window = (MoveGameCameraToEditorCamera)EditorWindow.GetWindow(typeof(MoveGameCameraToEditorCamera), true, "Capture Panorama...");
+            //window.Show();
+        }
+
+        void OnGUI()
+        {
+            if (GUILayout.Button("Teleport!")) {
+                
+            }
+                
+
+            this.Repaint();
+        }
+
+    }
+}
---
+++ Assets/PlusSharp/Scripts/VectorExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NUDev.PlusSharp {
+    public static class VectorExtension {
+        /// <summary>
+        /// Discards the Y value of the Vector.
+        /// </summary>
+        public static Vector3 discardY(this Vector3 yed) {
+            return new Vector3(yed.x, 0, yed.z);
+        }
+    }
+}

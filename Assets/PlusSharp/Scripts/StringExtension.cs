032e2889266f6765482bd9608278323b0146ba56
Narek Torosyan <narek.email@gmail.com>
2020-04-22 16:00:41 +0400

    Remove RandomString(string, int)
    Instead there is a default value for chars, so no breaking changes

--- Assets/PlusSharp/Scripts/StringExtension.cs
+++ Assets/PlusSharp/Scripts/StringExtension.cs
@@ -12,25 +12,13 @@ namespace NUDev.PlusSharp {
         /// <param name="length">The length of the string.</param>
         /// <param name="chars">What characters to include.</param>
         /// <returns>A random string.</returns>
-        public static string RandomString(this string str, int length, string chars) {
+        public static string RandomString(this string str, int length, string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
             var st = new string(Enumerable.Repeat(chars, length)
               .Select(s => s[Random.Range(0, s.Length)]).ToArray());
 			str = st;
 			return st;
         }
 
-        /// <summary>
-        /// Returns a random string. Don't use for secure stuff.
-        /// </summary>
-        /// <param name="length">The length of the string.</param>
-        /// <returns>A random string.</returns>
-        public static string RandomString(this string str, int length) {
-            var st = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length)
-              .Select(s => s[Random.Range(0, s.Length)]).ToArray());
-			str = st;
-			return st;
-        }
-        
         /// <summary>
         /// Split a string to equally sized chunks.
         /// </summary>

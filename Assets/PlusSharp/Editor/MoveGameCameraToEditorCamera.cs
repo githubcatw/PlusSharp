8120c184247fc52bd5e5556ec9774272b5159f84
Narek Torosyan <narek.email@gmail.com>
2020-04-22 15:49:35 +0400

    Update MoveGameCameraToEditorCamera.cs

--- Assets/PlusSharp/Editor/MoveGameCameraToEditorCamera.cs
+++ Assets/PlusSharp/Editor/MoveGameCameraToEditorCamera.cs
@@ -3,30 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-namespace NUDev.PlusSharp.Editor
-{
-    public class MoveGameCameraToEditorCamera : EditorWindow
-    {
-        [MenuItem("Window/Move Game camera to Scene View camera %m")]
-        // Use this for initialization
-        static void Init()
-        {
+namespace NUDev.PlusSharp.Editor {
+    public class MoveGameCameraToEditorCamera : EditorWindow {
+        [MenuItem("Window/Move main camera to Scene View camera %m")]
+        static void Init() {
             var cam = SceneView.lastActiveSceneView.camera;
             Camera.main.transform.position = cam.transform.position;
             Camera.main.transform.rotation = cam.transform.rotation;
-            //Capture window = (MoveGameCameraToEditorCamera)EditorWindow.GetWindow(typeof(MoveGameCameraToEditorCamera), true, "Capture Panorama...");
-            //window.Show();
         }
-
-        void OnGUI()
-        {
-            if (GUILayout.Button("Teleport!")) {
-                
-            }
-                
-
-            this.Repaint();
-        }
-
     }
 }
